45993d5355cec1e3db422faa637b8ff46801a16b
VitroidFPV <vitroid05@gmail.com>
2023-11-17 18:04:55 +0100

    Add testing comments

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,21 @@
 <script lang="ts">
+
+	// for testing:
+	// change the title value in the md file and save
+	// expected: console logs the new title on every save
+	// actual: console logs the new title on initial load and first file save,
+	// subsequent saves log the title from the first save
+
 	import { getModules } from '$lib/getModules';
 
-	getModules();
+	// getModules(); 
+	// imported function only logs the correct title on initial load and first file save
+
+	function getModules2() {
+		let modules = import.meta.glob("/modules/**/*.md", {eager: true});
+		console.log(modules["/modules/test.md"].metadata.title)
+	}
+
+	// getModules2();
+	// "local" function logs correct title on initial load and every file save
 </script>
